b88d99c6fa8ba26dd3912ad61aa520675a59d2e8
Nikolaos Ballas <nikolaosballas@eurofins.com>
2020-11-09 04:31:32 +0100

    Fixing some issues with the namespace

--- src/main/java/io/nexten/demo/NextenDemoApplication.java
+++ src/main/java/io/nexten/demo/NextenDemoApplication.java
@@ -3,7 +3,6 @@ package io.nexten.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.dekorate.docker.annotation.DockerBuild;
 import io.dekorate.kubernetes.annotation.KubernetesApplication;
 
 @SpringBootApplication
